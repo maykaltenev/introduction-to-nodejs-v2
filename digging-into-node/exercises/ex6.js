f77856e354b0ccce839a082aa78dfd31296bb932
Maykal Tenev <michaeltenev@gmail.com>
2022-09-08 16:26:28 +0200

    using express

--- digging-into-node/exercises/ex6.js
+++ digging-into-node/exercises/ex6.js
@@ -6,14 +6,14 @@ var util = require("util");
 var path = require("path");
 var http = require("http");
 
-// var express = require("express");
+var express = require("express");
 var sqlite3 = require("sqlite3");
 
-
+var app = express();
 // ************************************
 
-const DB_PATH = path.join(__dirname,"my.db");
-const WEB_PATH = path.join(__dirname,"web");
+const DB_PATH = path.join(__dirname, "my.db");
+const WEB_PATH = path.join(__dirname, "web");
 const HTTP_PORT = 8039;
 
 var delay = util.promisify(setTimeout);
@@ -23,8 +23,8 @@ var delay = util.promisify(setTimeout);
 var myDB = new sqlite3.Database(DB_PATH);
 var SQL3 = {
 	run(...args) {
-		return new Promise(function c(resolve,reject){
-			myDB.run(...args,function onResult(err){
+		return new Promise(function c(resolve, reject) {
+			myDB.run(...args, function onResult(err) {
 				if (err) reject(err);
 				else resolve(this);
 			});
