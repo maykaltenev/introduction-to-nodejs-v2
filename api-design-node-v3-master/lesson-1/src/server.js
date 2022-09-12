237db88658c6e5878dd346acb00a8d746822d986
Maykal Tenev <michaeltenev@gmail.com>
2022-09-12 18:41:29 +0200

    testing schema

--- api-design-node-v3-master/lesson-1/src/server.js
+++ api-design-node-v3-master/lesson-1/src/server.js
@@ -4,16 +4,26 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
+router.get('/me', (req, res) => {
+    res.send({ message: 'hello' })
+})
+router.route('/cat').get().post()
+router.route('/cat/:id').get().put().delete()
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+const log = ('/data', (req, res, next) => {
+    console.log('logging')
+    next()
+})
 app.get('/data', (req, res) => {
-    res.send({ message: 'hello' })
+    res.send({ data: [1, 2, 3] })
 })
 app.post('/data', (req, res) => {
     res.send(req.body)
